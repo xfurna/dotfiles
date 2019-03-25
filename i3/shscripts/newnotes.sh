dcfd97ae179ebf1d34785afa89c49ee9e255265d
Archit Dwivedi <architdwivedi.off@gmail.com>
2019-03-25 03:05:14 +0000

    Update newnotes.sh

--- i3/shscripts/newnotes.sh
+++ i3/shscripts/newnotes.sh
@@ -1,9 +1,11 @@
+# Script lets you open typora in your notes folder (required) with a [some number]new note.md file opened by default for you to just jump in and get started right away.
+
 #!/bin/bash
 IFS='
 '
-file_list=($(ls /hdd/Documents/Notes)) # sunbstitute the with your local Notes directory
+Notesdir="" # substitute this with your local Notes directory
+file_list=($(ls "${Notesdir}")) 
 file_count=(${#file_list[*]})
- echo "${file_list[*]}"
 nonote=0
 for (( i=0; i<${file_count}; i=$i+1))
 do
@@ -16,6 +18,6 @@ do
     fi
 done
 newnote="$((${i}-${nonote}+1))newnote.md"
-touch "/hdd/Documents/Notes/$newnote"
-typora "/hdd/Documents/Notes/$newnote"
+touch "${Notesdir}/$newnote"
+typora "${Notesdir}/$newnote"
 
