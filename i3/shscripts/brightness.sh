50dc28fb49b098ebdb78e398e52683856371eb39
evi1haxor <architdwivedi.off@gmail.com>
2020-01-25 17:33:14 +0530

    M  i3/config M  i3/shscripts/brightness.sh

--- i3/shscripts/brightness.sh
+++ i3/shscripts/brightness.sh
@@ -1,14 +1,14 @@
-
 #!/bin/bash
 
-max=$(pkexec /usr/lib/gnome-settings-daemon/gsd-backlight-helper --get-max-brightness)
-cur=$(pkexec /usr/lib/gnome-settings-daemon/gsd-backlight-helper --get-brightness)
-
-step=$((max/100+1))
+max=1
+cur=$(xrandr --verbose | grep "Brightness: " | cut -f2- -d' ')
 
 case "$1"
 in
-+) new=$((cur+step));;
--) new=$((cur-step));;
++) new=$(echo "$cur + 0.10" | bc);;
+-) new=$(echo "$cur - 0.10" | bc);;
 esac
-pkexec /usr/lib/gnome-settings-daemon/gsd-backlight-helper --set-brightness $new
+
+#if [ "$new" -ge 0 -a "$new" -le 1 ]; then echo "$new";
+#fi
+xrandr --output eDP-1-1 --brightness "$new"
